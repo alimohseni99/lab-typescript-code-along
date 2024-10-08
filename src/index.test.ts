dfa011626cf8e0da9ebcca5068c611968b656342
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-10-08 15:36:01 +0200

    uppercase with empty arr works

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,21 @@
 import { deepEqual } from "node:assert/strict";
 import test from "node:test";
 
-test("It works", () => {
-  deepEqual(1, 1);
+const data = [
+  { id: "1", name: "daniel" },
+  { id: "2", name: "ali" },
+];
+
+type Person = {
+  id: string;
+  name: string;
+};
+
+function uppercaseName(names: Person[]) {
+  return [];
+}
+
+test("upperCaseNames works with empty arr", () => {
+  const result = uppercaseName([]);
+  deepEqual(result, []);
 });
